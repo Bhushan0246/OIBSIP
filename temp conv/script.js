1c7141bb1a2440c6763279ac40451cc23fc78db0
Bhushan Harode <bhushanharode0246@gmail.com>
2023-08-01 22:47:56 +0530

    Add files via upload

--- temp conv/script.js
+++ temp conv/script.js
@@ -1,1 +1,6 @@
-getElementById('result').style.color="red";
+var y = document.getElementById("temp").value;
+let z = document.getElementById('which_temp');
+function convert() {
+let res = y * ( 9 / 5 ) + 32;
+document.getElementById("result").innerHTML = res;
+}
